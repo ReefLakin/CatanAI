8a3fc8cd5f349ea42603e4908fd249069ed765ed
Reef Lakin <58116295+ReefLakin@users.noreply.github.com>
2023-02-05 17:51:56 +0000

    Create QNetwork.py
    Added a Python class to represent the Q-Network. This is made using PyTorch with a simple constructor and forward method.

---
+++ machine-player/QNetwork.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_size, 32)
+        self.fc2 = nn.Linear(32, 64)
+        self.fc3 = nn.Linear(64, action_size)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
