127e2e35330bfbcddac5396d2f4c617697d7a092
Reef Lakin <58116295+ReefLakin@users.noreply.github.com>
2023-02-27 14:41:30 +0000

    Expanded action selection
    Expanded the select_action method, so actions can be taken based on the neural network output as well.

--- machine-player/Agent.py
+++ machine-player/Agent.py
@@ -1,17 +1,35 @@
 # Import the random library
 import random
 
+# Import the torch library
+import torch
+
+# Import the CatanModel class
+from Model import CatanModel
+
 # Define the Agent class
 class Agent:
     def __init__(self, exploration_rate):
+        # Set the exploration rate
         self.exploration_rate = exploration_rate
+        # Set the model (use the default values for now)
+        self.model = CatanModel()
 
     # Method for selecting an action
-    def select_action(self, legal_actions):
+    def select_action(self, observation, legal_actions):
         print(legal_actions)
+        # If the random number is less than the exploration rate, choose a random action
         if random.random() < self.exploration_rate:
-            return random.choice(legal_actions)
+            action = random.choice(legal_actions)
 
-        # Eventually to be replaced with a call to the model (at the moment exp. rate is going to always be 1)
+        # Otherwise, use the model to predict the best action
         else:
-            return random.choice(legal_actions)
+            # Convert the observation to a tensor
+            observation = torch.tensor(observation, dtype=torch.float32)
+            # Pass the observation through the model
+            action_options = self.model.forward(observation)
+            # Acquire the singular action with the highest value
+            action = torch.argmax(action_options).item()
+
+        # Return the action
+        return action
