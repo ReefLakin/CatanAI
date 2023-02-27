2c750a26b16980e264bffbc79aef3696412647cd
Reef Lakin <58116295+ReefLakin@users.noreply.github.com>
2023-02-27 12:14:51 +0000

    Add more flavour to weight init.
    The weights will be initialised by default by PyTorch, but here we use He initialisation.

--- machine-player/Model.py
+++ machine-player/Model.py
@@ -10,6 +10,19 @@ class CatanModel(nn.Module):
         self.relu2 = nn.ReLU()
         self.fc3 = nn.Linear(hidden_size, output_size)
 
+        # Initialise weights with the He uniform method
+        # Loop through all layers and initialise the weights
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                # a represents the slope of the negative part of the rectifier, default is 0
+                # mode is the method of initialisation
+                # nonlinearity is the activation function
+                nn.init.kaiming_uniform_(
+                    m.weight, a=0, mode="fan_in", nonlinearity="relu"
+                )
+                # Initialise biases to zero, which is the default anyway
+                nn.init.zeros_(m.bias)
+
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu1(x)
