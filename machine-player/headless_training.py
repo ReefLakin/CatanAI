e65b11052e4556c8b2eb2f6db4239c82814e941a
Reef Lakin <58116295+ReefLakin@users.noreply.github.com>
2023-03-08 16:45:34 +0000

    Added CSV analysis output after training session
    After each session of training, we'll collect some CSV data for analysis purposes.

--- machine-player/headless_training.py
+++ machine-player/headless_training.py
@@ -15,6 +15,12 @@ from Adam import Adam
 # REPLAY MEMORY
 from ReplayMemory import ReplayMemory
 
+# DATETIME
+from datetime import datetime
+
+# CSV
+import csv
+
 
 # Set Constants
 
@@ -66,6 +72,19 @@ running = True
 learn_steps = 0
 games_played = 0
 
+# Other data about the current session
+total_illegal_actions_attempted = 0
+total_steps_taken = 0
+total_reward_points_earned = 0
+total_roads_built = 0
+
+# Other data about the current session, but these are arrays
+total_illegal_actions_attempted_list = []
+total_steps_taken_list = []
+total_reward_points_earned_list = []
+total_roads_built_list = []
+game_numbers_list = []
+
 while running is True and games_played != EPISODES:
 
     # Get the legal actions from the game instance
@@ -80,11 +99,23 @@ while running is True and games_played != EPISODES:
     # Get the agent to pick an action
     action = agent.select_action(game_state, all_actions, actions)
 
+    # If the action is illegal, increment the illegal action counter
+    if action not in actions:
+        total_illegal_actions_attempted += 1
+    else:
+        # If the action is to build a road, increment the road counter
+        split_action = action.split("_")
+        if split_action[0] == "build" and split_action[1] == "road":
+            total_roads_built += 1
+
     # What is the index of the action when set against the entire list of actions?
     action_index = all_actions.index(action)
 
     # Take a step in the game
     game_instance.step(action)
+
+    # Increment the total number of steps taken
+    total_steps_taken += 1
 
     # Get the new game state
     new_game_state = game_instance.get_state()
@@ -97,6 +128,9 @@ while running is True and games_played != EPISODES:
 
     # Get the reward from the Agent
     reward = agent.reward(reward_information)
+
+    # Increment the total reward points earned
+    total_reward_points_earned += reward
 
     # Create a memory tuple
     memory_tuple = (game_state, action_index, reward, new_game_state)
@@ -119,13 +153,62 @@ while running is True and games_played != EPISODES:
     game_over_flag = game_instance.get_game_over_flag()
 
     if game_over_flag == True:
+        # Append the CSV data to the arrays
+        total_illegal_actions_attempted_list.append(total_illegal_actions_attempted)
+        total_steps_taken_list.append(total_steps_taken)
+        total_reward_points_earned_list.append(total_reward_points_earned)
+        total_roads_built_list.append(total_roads_built)
+        game_numbers_list.append(games_played)
+
         games_played += 1  # Increment the number of games played
-        if games_played == EPISODES:
+        if games_played == EPISODES or games_played == 12000:
+            games_played = 0
             replay_memory.save_buffer(AGENT_SELECTED)
-            print("Saved as JSON!")
+
+            data = zip(
+                game_numbers_list,
+                total_illegal_actions_attempted_list,
+                total_steps_taken_list,
+                total_reward_points_earned_list,
+                total_roads_built_list,
+            )
+
+            # Get current date and time for file name
+            now = datetime.now()
+            filename = (
+                "training_session_data/"
+                + AGENT_SELECTED
+                + now.strftime("-%Y-%m-%d-%H-%M")
+                + ".csv"
+            )
+
+            # Open a file for writing
+            with open(filename, "w", newline="") as csvfile:
+                writer = csv.writer(csvfile)
+
+                # Write the header row containing the array names
+                writer.writerow(
+                    [
+                        "Game number",
+                        "Total illegal actions attempted",
+                        "Total steps taken",
+                        "Total reward points",
+                        "Roads built",
+                    ]
+                )
+
+                # Write the data rows
+                for row in data:
+                    writer.writerow(row)
 
         # Reset the game
         game_instance.reset()
+
+        # Reset other data about the current session
+        total_illegal_actions_attempted = 0
+        total_steps_taken = 0
+        total_reward_points_earned = 0
+        total_roads_built = 0
 
         # Start the game
         game_instance.start_game()
